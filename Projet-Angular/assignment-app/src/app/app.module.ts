69700b8b5bc7ac695f02a8ee3301355b9ba4668f
Nouke souare <nsouare2006@gmail.com>
2022-09-22 15:11:41 +0200

    Exemple avec material angular

--- Projet-Angular/assignment-app/src/app/app.module.ts
+++ Projet-Angular/assignment-app/src/app/app.module.ts
@@ -5,7 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
-
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
 @NgModule({
   declarations: [
     AppComponent
@@ -14,7 +15,7 @@ import {MatButtonModule} from '@angular/material/button';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,MatIconModule, MatDividerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
