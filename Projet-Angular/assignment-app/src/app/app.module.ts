c626767009a4687413a46cc6b6dbf71fec64d20a
Nouke souare <nsouare2006@gmail.com>
2022-09-22 15:40:53 +0200

    assignments works

--- Projet-Angular/assignment-app/src/app/app.module.ts
+++ Projet-Angular/assignment-app/src/app/app.module.ts
@@ -7,9 +7,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
+import { AssignmentsComponent } from './assignments/assignments.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AssignmentsComponent
   ],
   imports: [
     BrowserModule,
