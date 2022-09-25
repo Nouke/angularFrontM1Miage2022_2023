f7227477d28d05f72ce2c28e1f77bc213cad8a63
Nouke souare <nsouare2006@gmail.com>
2022-09-26 00:34:35 +0200

    Directives structurelles *ngFor

--- Projet-Angular/assignment-app/src/app/assignments/assignments.component.ts
+++ Projet-Angular/assignment-app/src/app/assignments/assignments.component.ts
@@ -7,6 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./assignments.component.css']
 })
 export class AssignmentsComponent implements OnInit {
+  titre = "Mon application sur les Assignments !"
+
+  assignments = [
+    {
+      nom: " TP1 Analyse de données à rendre",
+      dateDeRendu: '2022-10-10',
+      rendu: true
+
+    },
+
+    {
+      nom: "TP2 Angular à rendre",
+      dateDeRendu: '2022-10-15',
+      rendu: true
+
+    },
+
+    {
+      nom: "TP3 Mini Projet Angular à rendre",
+      dateDeRendu: '2022-10-18',
+      rendu: true
+
+    }
+  ]
 
   constructor() { }
 
